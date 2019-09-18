3dde37a19008e4939e365842a094c04068fc89bb
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-18 18:43:05 -0300

    Inclusão correta da Toolbar na Activity desejada

--- app/src/main/java/com/example/themematerialdesign/ToolbarActivity.kt
+++ app/src/main/java/com/example/themematerialdesign/ToolbarActivity.kt
@@ -2,20 +2,29 @@ package com.example.themematerialdesign
 
 import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+
 
 class ToolbarActivity : AppCompatActivity() {
 
     private var context : Context? = null
-//    private lateinit var toolbar : Toolbar
+    private lateinit var toolbar : Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toolbar)
 
+        setSupportActionBar(findViewById(R.id.myToolbar))
+
         initVars()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
     }
 
     private fun initVars(){
