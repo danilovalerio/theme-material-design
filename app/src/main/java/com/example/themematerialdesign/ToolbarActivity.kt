3d3a3d19ae7c8ff447c3a3aee76bdab19b5d493a
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-18 19:15:45 -0300

    Verificando item selecionado no MENU com o onOptionsItemSelected

--- app/src/main/java/com/example/themematerialdesign/ToolbarActivity.kt
+++ app/src/main/java/com/example/themematerialdesign/ToolbarActivity.kt
@@ -3,8 +3,10 @@ package com.example.themematerialdesign
 import android.content.Context
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.themematerialdesign.extensions.toast
 
 
 class ToolbarActivity : AppCompatActivity() {
@@ -26,6 +28,20 @@ class ToolbarActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
         return true
+    }
+
+    //verifica qual item do menu foi clicado
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.buscar_menu -> toast("Buscar selecionado")
+            R.id.carrinho_menu -> toast("Carrinho selecionado")
+            R.id.config_menu -> toast("Configurações selecionado")
+            R.id.sobre_menu -> toast("Sobre selecionado")
+
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun initVars(){
